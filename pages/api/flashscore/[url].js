5ca5aa24ac6394cca6b0e3f7ecdc7d5e9a28802d
MutsuoYoshiichi <411.mutsuo@gmail.com>
2022-05-06 22:16:10 +0900

    first

---
+++ pages/api/flashscore/[url].js
@@ -0,0 +1,54 @@
+
+const puppeteer = require('puppeteer');
+
+
+
+
+export default async function getTeamScors(req, res) {
+	try {
+		let url = "https://www.flashscore.co.jp/team/schio/h4Y1lKRQ/";
+		url = req.query.url
+
+		// (async () => {
+		const options = {
+			// headless: false, // ヘッドレスをオフに
+			// slowMo: 100  // 動作を遅く
+		};
+		const browser = await puppeteer.launch(options);
+		const page = await browser.newPage();
+		await page.goto(url);
+
+		setTimeout(async () => {
+			const html = await page.content()
+			res.json({ html })
+		}, 3000);
+
+	} catch (error) {
+		console.error(error)
+		res.status(500).json({ error: error })
+	}
+}
+
+
+
+
+async function getText(elem) {
+	let result;
+	let jsHadnle = await elem.getProperty('textContent');
+	result = await jsHadnle.jsonValue();
+	return result;
+}
+
+
+//**親要素の中でselector検索を行い、テクストを返す */
+async function getTextFromElementArray(parent, selector) {
+	try {
+		const elems = await parent.$$(selector);
+		const elem = elems[0]
+		return await getText(elem)
+	} catch (error) {
+		console.error(error)   //////////
+		return 'error'
+	}
+
+}
