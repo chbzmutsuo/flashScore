9325d1c8a2597251583d37bb8048f4e8af0898e0
chbzmutsuo <411.mutsuo@gmail.com>
2022-05-25 10:39:33 +0900

    shin-okadaさま完成

--- pages/api/shin-okada/[...query].js
+++ pages/api/shin-okada/[...query].js
@@ -354,9 +354,12 @@ const scrapeAmazon = async (searchWord) => {
 
 			items.push({ title, price, href, imageUrl })
 			console.log({ items })   //////////
-
-
 		})
+
+
+
+
+
 		let removeNoPrice = removeNoPriceItem(items)
 		return { url: URL, data: sortByPrice(removeNoPrice) }
 	}).catch(error => { console.error(error); return { msg: error } })
@@ -404,7 +407,7 @@ const scrapeSurugaya = async (searchWord) => {
 			href = $(this).find('a').attr('href');
 			$(this).find('.price_teika').each(function () {
 				let replacedText = $(this).text().replace(/\t|\r|\n|\s/g, "");
-				replacedText.includes("新品") ?? replacedText.includes("予約") ?? replacedText.includes("定価") ? price = Number(replacedText.replace(/\D/g, "")) : '';
+				replacedText.includes("新品") ?? replacedText.includes("予約") ? price = Number(replacedText.replace(/\D/g, "")) : '';
 			})
 			// price = price.replace(/\t|\r|\n|\s/g, "");
 			// price = price.replace(/¥|,|\n|\(税込\)|代引・銀行・コンビニ|\s|/g, text => { return text })
